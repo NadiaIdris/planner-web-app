16a36c44d05566dd2d541ac071ba85a9982493fe
Maret Idris <maret@fasterlap.com>
2019-05-15 17:11:06 -0700

    Major refactor
    - Move dangling lines of code in main.js to the main function (mostly
      event listeners).
    - Validate that the task text is not empty
    - Checkbox checked and unchecked on click
    - Clean up the task object by removing deadlineFormatted
    - Add priority to a task
    - Add current date and time

--- main.js
+++ main.js
@@ -2,14 +2,30 @@
 
 const formElement = document.querySelector('#form');
 
-const onDomLoad = () => {
+const main = () => {
+  loadFromLocalStorage();
   formElement.addEventListener('submit', addTask);
   createEmptyState();
+  window.addEventListener('load', paintScreen);
+  initializePlannerUI();
+  initializeDoneUI();
+  tasksContainer.addEventListener('change', selectPriority);
+  tasksContainer.addEventListener('click', toggleDone);
+  tasksContainer.addEventListener('click', deleteTask);
+  tasksContainer.addEventListener('change', addDeadline);
+  tasksContainer.addEventListener('focusout', editTaskText);
+  generateTodaysDateAndTime();
+  checkboxButton.addEventListener('click', viewDoneTasks);
+  handleWindowResize();
+  tasksContainer.addEventListener('keydown', keydownListener);
 };
 
 // Tasks.
 
-const tasks = [];
+let tasks;
+const loadFromLocalStorage = () => {
+  tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+};
 
 /**
  * Create a task table given an event. The event is generated on form submit.
@@ -17,17 +33,38 @@ const tasks = [];
  */
 const addTask = (event) => {
   event.preventDefault();
-  // Store the value in an object inside an array in local storage.
+  // Store the value in an object. Store object inside an array in local
+  // storage.
   const text = document.querySelector('#add-task').value;
+
+  // If text field is empty, stop executing the rest of the function.
+  if (checkIfTaskIsEmpty(text)) return;
+
   const task = {
     text,
     done: false,
+    priority: "P2",
     deadline: undefined,
   };
+
   tasks.push(task);
   formElement.reset();
+  localStorage.setItem('tasks', JSON.stringify(tasks));
   generateTableWithHeader();
   generateListOfTasks(tasks);
+};
+
+/**
+ * Check if task entered is empty.
+ * @param taskText
+ * @returns {boolean} true
+ */
+const checkIfTaskIsEmpty = (taskText) => {
+  if (taskText.trim() === '') {
+    alert("Task is empty");
+    formElement.reset();
+    return true;
+  }
 };
 
 // Tasks container.
@@ -42,7 +79,7 @@ const generateTableWithHeader = () => {
   const tasksTable = document.querySelector('#tasks-table');
   if (!tasksTable) {
     // clear the empty state
-    deleteElementById('empty-stage-planner');
+    deleteElementBySelector('#empty-stage-planner');
 
     const table = document.createElement('table');
     table.setAttribute('id', 'tasks-table');
@@ -51,9 +88,9 @@ const generateTableWithHeader = () => {
                 <thead>
                 <tr id="task-headings">
                     <th></th>
-                    <th class="heading-cell">Tasks</th>
-                    <th class="heading-cell">Deadlines</th>
-                    <th></th>
+                    <th class="heading-cell">Task</th>
+                    <th class="heading-cell"><i class="material-icons arrow-down">keyboard_arrow_down</i>Priority</th>
+                    <th class="heading-cell">Deadline</th>
                     <th></th>
                 </tr>
                 </thead>`;
@@ -74,24 +111,62 @@ const generateListOfTasks = (tasksArray = []) => {
 
   // Map over each array element and paint them on screen.
   tableBody.innerHTML = tasksArray.map((task, index) => {
+    const deadlineAttributeHTML = task.deadline ? `value="${task.deadline}"` : '';
     return `
        <tr class="task">
-          <td class=""chkbx-cell><input type="checkbox"></td>
-           <td><input type="text" class="task-cell" value="${task.text}"></td>
-           <td><div class="deadline">${task.deadline}</div></td>
-           <td class="icon-cell"><i class="material-icons">delete</i></td>
-           <td class="img-cell"><img src="images/move-icon.svg" alt="move tasks icon"></td>
+           <td class="chkbx-cell">
+             <img 
+               src="${task.done ? `images/checkbox-checked.svg` : `images/checkbox-unchecked.svg`}" 
+               data-index="${index}"></td>
+           <td><input type="text" class="task-cell" value="${task.text}" data-index="${index}"></td>
+           <td class="priority-cell">
+              <select class="priority" data-index="${index}">
+                    <option value="P0" ${task.priority === "P0" ? "selected" : ""}>P0</option>
+                    <option value="P1" ${task.priority === "P1" ? "selected" : ""}>P1</option>
+                    <option value="P2" ${task.priority === "P2" ? "selected" : ""}>P2</option>
+              </select></td>
+           <td class="deadline-cell">
+             <input type="date" class="deadline" ${deadlineAttributeHTML} data-index="${index}">
+           </td>
+           <td class="icon-cell">
+             <i class="material-icons" data-index="${index}">delete</i>
+           </td>
        </tr>
        `;
   }).join('');
 };
 
-// Empty state.
+const selectPriority = (event) => {
+  const element = event.target;
+  const index = element.dataset.index;
+  if (!element.matches('.priority')) return;
+
+  if (element.value === "P0") {
+    tasks[index].priority = "P0";
+  } else if (element.value === "P1") {
+    tasks[index].priority = "P1";
+  } else if (element.value === "P2") {
+    tasks[index].priority = "P2";
+  }
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
 
 /**
- * If no tasks created, run addEmptyStateToPlanner() function.
+ * If item(s) in tasks, then generate table with the task(s).
+ */
+const initializePlannerUI = () => {
+  if (tasks.length === 0) return;
+  generateTableWithHeader();
+  generateListOfTasks(tasks);
+};
+
+/**
+ * If no tasks created, then paint the empty state into on planner page.
  */
 const createEmptyState = () => {
+  if (tasks.length > 0) return;
   const tasksTable = document.querySelector('#tasks-table');
   if (!tasksTable) {
     addEmptyStateToPlanner();
@@ -111,7 +186,99 @@ const addEmptyStateToPlanner = () => {
   div.innerHTML = `<img class="sun" src="images/sun.svg"><p class="empty-stage-text gray">You have no tasks.<br>Add a task below.</p>`;
 };
 
+// Function to toggle done and undone. Still needs work!
+const toggleDone = event => {
+  const element = event.target;
+  const index = element.dataset.index;
+  if (!element.matches(`img[data-index="${index}"]`)) return;
+  tasks[index].done = !tasks[index].done;
 
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  generateListOfTasks(tasks);
+};
+
+// Function to delete a task.
+function deleteTask(event) {
+  const element = event.target;
+  const index = element.dataset.index;
+  // Only register the click on delete icon.
+  if (!element.matches('.icon-cell i.material-icons')) return;
+  tasks.splice(`${index}`, 1);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  generateListOfTasks(tasks);
+
+  if(tasks.length === 0){
+    deleteElementBySelector('#tasks-table');
+    createEmptyState();
+  }
+}
+
+
+function addDeadline(event) {
+  // debugger;
+  const element = event.target;
+  const index = element.dataset.index;
+  if (!element.matches('.deadline-cell input[type="date"]')) return;
+
+  const dateInShort = element.value;
+  tasks[index].deadline = dateInShort;
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  generateListOfTasks(tasks);
+}
+
+// Function to store edited task value in local storage.
+const editTaskText = (event) => {
+  const element = event.target;
+  const text = element.value;
+  const index = element.dataset.index;
+  if (!element.matches('.task-cell')) return;
+  tasks[index].text = text;
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+
+// Function to store edited task value in local storage on click "Enter".
+
+const keydownListener = (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+
+    const element = event.target;
+    const text = element.value;
+    const index = element.dataset.index;
+    if (!element.matches('.task-cell')) return;
+    tasks[index].text = text;
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    // generateTableWithHeader();
+    // generateListOfTasks(tasks); if I add this, it will brake the code. Why??
+    element.blur();
+  }
+};
+
+
+// Function to add a current date on the website.
+const generateTodaysDateAndTime = () => {
+  setInterval(() => {
+    const dateContainer = document.querySelector('#date');
+    const today = new Date();
+
+    const date = today.toLocaleDateString('en-US', {
+      month: 'long',
+      day: 'numeric'
+    });
+
+    const time = today.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+
+    dateContainer.innerHTML = `Today \u00A0\u00A0|\u00A0\u00A0 ${date} \u00A0\u00A0|\u00A0\u00A0 ${time}`;
+  }, 1000);
+};
 
 
 // TODO Clean up code below.
@@ -130,26 +297,18 @@ const addEmptyStateToDone = () => {
 
 // If no tasks completed, run addEmptyStateToDone() function
 const tasksDone = document.querySelector('#tasks-done');
-if (!tasksDone) {
-  addEmptyStateToDone();
-}
-
-
-
-
-
+const initializeDoneUI = () => {
+  if (!tasksDone) {
+    addEmptyStateToDone();
+  }
+};
 
 // Responsive design JS
 
 
 const delay = 1;
 
-const originalResize = () => {
-  paintScreen();
-};
-
-
-(() => {
+const handleWindowResize = () => {
   let resizeTaskId = null;
 
   window.addEventListener('resize', evt => {
@@ -159,10 +318,10 @@ const originalResize = () => {
 
     resizeTaskId = setTimeout(() => {
       resizeTaskId = null;
-      originalResize(evt);
+      paintScreen();
     }, delay);
   });
-})();
+};
 
 
 const paintScreen = () => {
@@ -174,7 +333,7 @@ const paintScreen = () => {
   const doneContainer = document.querySelector('#done-container');
   const mainContent = document.querySelector('#main-content');
 
-  if (window.matchMedia("(min-width: 720px)").matches) {
+  if (window.matchMedia("(min-width: 800px)").matches) {
     addButton.style.display = 'flex';
     checkboxButton.style.display = 'none';
     addButtonSmall.style.display = 'none';
@@ -187,7 +346,7 @@ const paintScreen = () => {
 
   }
 
-  if (window.matchMedia("(max-width: 719px)").matches) {
+  if (window.matchMedia("(max-width: 799px)").matches) {
     addButton.style.display = 'none';
     checkboxButton.style.display = 'flex';
     addButtonSmall.style.display = 'flex';
@@ -198,14 +357,12 @@ const paintScreen = () => {
 
   }
 
-  if (window.matchMedia("(max-width: 719px)").matches && checkboxClicked === true) {
+  if (window.matchMedia("(max-width: 799px)").matches && checkboxClicked === true) {
     doneContainer.style.display = 'flex';
     mainContent.style.display = 'none';
     console.log("screen width < 719 && checkboxClicked === true")
   }
 };
-
-window.addEventListener('load', paintScreen);
 
 
 // Function to view done tasks if screen is smaller then 720px;
@@ -228,8 +385,6 @@ const viewDoneTasks = () => {
   mainContent.style.display = 'none';
 };
 
-checkboxButton.addEventListener('click', viewDoneTasks);
-
 
 // Run on document loaded.
-document.addEventListener("DOMContentLoaded", onDomLoad);
+document.addEventListener("DOMContentLoaded", main);
--- util.js
+++ util.js
@@ -1,10 +1,3 @@
-const deleteElementById = (elementId) => {
-  if (!elementId) return;
-  const divToRemove = document.getElementById(elementId);
-  if (!divToRemove) return;
-  divToRemove.parentNode.removeChild(divToRemove);
-};
-
 const deleteElementBySelector = (selector) => {
   if (!selector) return;
   const divToRemove = document.querySelector(selector);
