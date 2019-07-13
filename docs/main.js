fb9266172f67e8af0ab4c26d0a28972f0e21287a
Maret Idris <maret@fasterlap.com>
2019-07-13 11:30:00 -0700

    Update README

--- docs/main.js
+++ docs/main.js
@@ -1164,7 +1164,7 @@ const generateListOfTasksDone = (tasksDoneArray = []) => {
         <td class="chkbx-cell-done">
           <img
              class="chkbx-img-checked"
-             src="../images/checkbox-checked.svg"
+             src="images/checkbox-checked.svg"
              data-index="${index}">
         </td>
         <td>
@@ -1206,7 +1206,7 @@ const generateListOfTasks = (tasksArray = []) => {
         <td class="chkbx-cell">
           <img
             class="chkbx-img-unchecked"
-            src="../images/checkbox-unchecked.svg"
+            src="images/checkbox-unchecked.svg"
             data-index="${index}">
         </td>
         <td class="textarea-cell">
@@ -1270,7 +1270,7 @@ const addEmptyStateToDone = () => {
   container.appendChild(div);
 
   div.innerHTML =
-      '<img class="checkbox" src="../images/checkbox_icon.svg">' +
+      '<img class="checkbox" src="images/checkbox_icon.svg">' +
       '<p class="empty-stage-text">' +
       'Tasks you get done<br>will appear here.</p>';
 };
@@ -1286,7 +1286,7 @@ const addEmptyStateToPlanner = () => {
   div.setAttribute('class', 'empty-stage');
 
   div.innerHTML =
-      '<img class="sun" src="../images/sun.svg">' +
+      '<img class="sun" src="images/sun.svg">' +
       '<p class="empty-stage-text gray">' +
       'You have no tasks.<br>Add a task below.</p>';
 };
